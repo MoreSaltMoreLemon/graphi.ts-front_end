2320fc894b01d2e384ed73631770f51662920037
omfgtora <spaminol@gmail.com>
2019-03-26 10:17:34 -0700

    Push it

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,25 @@ function main() {
     const sine = g.genFn(Math.sin, { x: 0, y: 200 }, canvas.width, 200, 50, 5);
     g.drawLine(sine, "blue");
     // end
-
     let mobileNav = document.querySelector('.mobile-nav');
     mobileNav.addEventListener('click', menuToggle);
+
+    window.addEventListener('hashchange', () => {
+        console.log('you are now viewing a new page')
+    })
+
+    let examples = document.querySelectorAll('.links a')
+    examples.forEach((link) => { link.addEventListener('click', (evnt) => {
+        let hash = evnt.target.hash.split('#')
+        let url = hash[1]
+        if (url) {
+            fetch(`/pages/${url}.html`).then(results => results.text())
+           .then(text => {
+            let page = document.getElementById('content')
+                page.innerHTML = text
+            })
+        }
+    })})
 }
 
 function menuToggle() {
