e948284b4c42792b07000c1f6b0bbb00a66b9eaa
github username <github email address>
2019-03-26 15:01:53 -0700

    changed theme, got Ace editor working

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,48 +1,123 @@
 class Editor {
-  constructor(editor, canvas) {
-    this.editor = editor;
-    this.canvas = editor;
-    this.context = canvas.getContext('2d');
-  }
 
+  constructor () {
+    this.editor;
+    this.canvas;
+  }
+  
   renderCanvasAndEditor() {
     const content = document.getElementById('content');
-    content.appendChild(this.renderCanvas());
-    content.appendChild(this.renderEditor());
-  }
-
-  renderCanvas() {
-    const canvas = document.createElement('canvas');
+    
+    const canvas = document.querySelector('canvas');
     canvas.id = 'graph';
-    return canvas;
-  }
+    this.canvas = canvas;
 
-  renderEditor() {
     const section = document.createElement('section');
     section.id = 'text-editor-form';
 
     const editor = document.createElement('div');
     editor.id = 'editor';
+    // editor.style.height = "400px";
+    // editor.style.width = "900px";
 
-    const select = document.createElement('section');
+    const select = document.createElement('select');
     select.id = 'graph-type';
 
-    const updateButton = document.createElement('button');
-    updateButton.id = 'update';
+    const runCodeButton = document.createElement('button');
+    runCodeButton.id = 'run';
+    runCodeButton.textContent = "Run Code";
+    runCodeButton.addEventListener('click', (e)=> this.updateGraph());
 
-    const clearButton = document.createElement('button');
-    clearButton.id = 'clear';
+    const copyButton = document.createElement('button');
+    copyButton.id = 'copy';
+    copyButton.textContent = "Copy";
+    copyButton.addEventListener('click', () => this.copyEditorContents())
 
     const saveButton = document.createElement('button');
     saveButton.id = 'save';
+    saveButton.textContent = "Save Image";
+    saveButton.addEventListener('click', () => this.saveImage());
 
     section.appendChild(editor);
     section.appendChild(select);
-    section.appendChild(updateButton);
-    section.appendChild(clearButton);
+    section.appendChild(runCodeButton);
+    section.appendChild(copyButton);
     section.appendChild(saveButton);
 
-    return section;
+    content.appendChild(canvas);
+    content.appendChild(section);
+    console.log("OHHHH YEAH")
   }
 
+  initializeEditor() {
+    this.editor = ace.edit('editor');
+    this.editor.session.setMode("ace/mode/javascript");
+    this.editor.setTheme("ace/theme/monokai");
+    // this.editor.setValue("the new text here");
+  }
+
+  populateSelectOptions(examples) {
+    const select = document.getElementById('graph-type');
+  
+    examples.sort((a, b) => a.graph_type < b.graph_type ? -1 : 1)
+            .forEach(ex => {
+              const option = document.createElement('option');
+              option.id = ex.graph_type;
+              option.value = ex.title;
+              option.textContent = ex.title;
+          
+              
+          
+              select.appendChild(option);
+            });
+  
+    select.addEventListener('change', (e) => {
+      const example = examples.find(ex => ex.title === select.value);
+      this.updateEditor(example.javascript);
+      this.updateGraph();
+    })
+  
+    const initialGraphExample = examples.find(ex => ex.title === select.value)
+    // console.log(initialGraphExample);
+    this.updateEditor(initialGraphExample.javascript);
+    this.updateGraph(initialGraphExample.javascript); 
+  }
+
+  updateGraph() {
+    this.clearCanvas();
+    const input = this.editor.getValue();
+    eval(input);
+  }
+
+  updateEditor(textContent) {
+    this.editor.setValue(textContent);
+    this.editor.clearSelection();
+  }
+
+  copyEditorContents() {
+    this.editor.selectAll()
+    document.execCommand("copy");
+    this.editor.clearSelection();
+  }
+
+  clearCanvas() {
+    const context = this.canvas.getContext('2d');
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  saveImage() {
+    const img = this.canvas.toDataURL();
+    // const image = document.createElement('img');
+    // image.src = img;
+    this.saveBase64AsFile(img, "test.png");
+  }
+
+  saveBase64AsFile(base64, fileName) {
+
+    var link = document.createElement("a");
+
+    link.setAttribute("href", base64);
+    link.setAttribute("download", fileName);
+    link.click();
+  }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,117 +4,35 @@ window.addEventListener("load", main);
 async function main() {
   const examples = await getExamples();
   
-  populationSelectFromExamples(examples);
-  initializeGraphEditorForm();
-  initializeHamburger();
-
-  updateGraph();
-
+  const editor = new Editor();
+  editor.renderCanvasAndEditor();
+  editor.initializeEditor();
+  editor.populateSelectOptions(examples);
+  editor.updateGraph();
   
-  var editor = ace.edit("editor");
-  
-  editor.session.setMode("ace/mode/javascript");
-  editor.setTheme("ace/theme/twilight");
-  
-  editor.setValue("the new text here");
-  // editor.session.setValue("the new text here"); // set value and reset undo history
-  console.log(editor.getValue()); 
+  initializeMenu();
 }
-
-
-function getInput() {
-  let input = document.getElementById('input');
-  return input.value
-}
-
-function setInput(inputText) {
-  let input = document.getElementById('input');
-  input.value = inputText;
-}
-
 
 async function getExamples() {
   return await fetch(EXAMPLES_URL)
     .then(response => response.json())
 }
 
-function populationSelectFromExamples(examples) {
-  const select = document.getElementById('graph-type');
-
-  examples.sort((a, b) => a.graph_type < b.graph_type ? -1 : 1)
-          .forEach(ex => {
-            const option = document.createElement('option');
-            option.id = ex.graph_type;
-            option.value = ex.title;
-            option.textContent = ex.title;
-        
-            
-        
-            select.appendChild(option);
-          });
-
-  select.addEventListener('change', (e) => {
-    const example = examples.find(ex => ex.title === select.value);
-    setInput(example.javascript);
-    updateGraph();
-  })
-
-  const initialGraphExample = examples.find(ex => ex.title === select.value)
-  setInput(initialGraphExample.javascript);
-
-  
-}
-
-
-function initializeGraphEditorForm() {
-  let canvas = document.querySelector("canvas");
-
-  updateGraph();
-
-  let updateButton = document.getElementById('input-button');
-  updateButton.addEventListener('click', (e)=>{
-      updateGraph();
-  });
-
-  let clearButton = document.getElementById('clear');
-  clearButton.addEventListener('click', () => clearCanvas(canvas))
-
-
-  let saveButton = document.getElementById('save');
-  saveButton.addEventListener('click', () => {
-      console.log("SAVE BUTTON:", getInput())
-  })
-}
-
-function clearCanvas(canvas) {
-  const context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
-
-function updateGraph() {
-  let canvas = document.querySelector("canvas");
-  clearCanvas(canvas)
-  const input = getInput()
-  eval(input);
-}
-
-
-
-
-function initializeHamburger() {
+function initializeMenu() {
   // Hamburger Toggle
   let mobileNav = document.querySelector('.mobile-nav');
   mobileNav.addEventListener('click', menuToggle);
+  menuLinks();
+}
 
-  window.addEventListener('hashchange', () => {
-    console.log('you are now viewing a new page');
-  })
+function menuToggle() {
+    let nav = document.querySelector('.links');
+    nav.classList.toggle('menu-closed');
+}
 
-  let examples = document.querySelectorAll('.links a')
-  examples.forEach((link) => { 
-    link.addEventListener('click', (e) => {
-      let hash = e.target.hash.split('#');
+function menuLinks() {
+  window.addEventListener('hashchange', (e) => {
+    let hash = location.hash.split('#');
       let url = hash[1];
       if (url) {
         fetch(`/pages/${url}.html`).then(results => results.text())
@@ -123,12 +41,5 @@ function initializeHamburger() {
             page.innerHTML = text;
           })
       }
-    })
   })
 }
-
-function menuToggle() {
-    let nav = document.querySelector('.links');
-    nav.classList.toggle('menu-closed');
-}
-
